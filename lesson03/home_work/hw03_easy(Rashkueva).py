52752ca501eb38ce95d9937a619e06f4f3ab8b62
rolga <glyinderwust@gmail.com>
2017-07-31 21:08:48 +0300

    Update hw03_easy(Rashkueva).py

--- lesson03/home_work/hw03_easy(Rashkueva).py
+++ lesson03/home_work/hw03_easy(Rashkueva).py
@@ -5,10 +5,28 @@
 # Для решения задачи не используйте встроенные и функции и функции из модуля math
 
 
-def my_round(number, ndigits):
-    pass
+print('Задача 1')
 
-my_round(2.1234567, 5)
+
+def my_round(number, ndigits):
+    number = str(number)
+    n, afterdot = number.split('.')
+    """Здесь мы проверяем, нужно ли вообще округлять введенное число"""
+    if len(afterdot) <= ndigits:
+        return number
+    else:
+        """'Собираем' число без точки для дальнейшей арифметики"""
+        slice = list(n + afterdot[:ndigits])
+        """Здесь мы проверяем, пойдет ли округление вверх или вниз"""
+        if int(afterdot[(ndigits - 1)]) >= 5:
+            slice = list(int(slice) + 1)
+        """Возвращаем число, вставляя точку на нужную позицию.
+        Позицию находим, исходя из требуемого количества символов послее нее"""
+        return float(''.join(slice.insert((len(slice) - ndigits), '.')))
+
+number = float(input())
+ndigits = int(input())
+print(my_round(number, ndigits))
 
 # Задание-2:
 # Дан шестизначный номер билета, определить является ли билет счасливым
@@ -23,11 +41,10 @@ def lucky_ticket(ticket_number):
     ticket_number = str(ticket_number)
     first_number = int(ticket_number[0]) + int(ticket_number[1]) + int(ticket_number[2])
     second_number = int(ticket_number[3]) + int(ticket_number[4]) + int(ticket_number[5])
-    if first_number == second_number:
-        return 'Счастливый билет!'
-    else:
-        return 'В следующий раз повезет больше'
+    return first_number == second_number
 
 ticket_number = input()
-print(lucky_ticket(ticket_number))
-
+if lucky_ticket(ticket_number):
+    print('Счастливый билет!')
+else:
+    print('В следующий раз повезет')
