19687581e9127187965e96671f1e0b5520f7cb9a
rolga <glyinderwust@gmail.com>
2017-08-04 13:40:50 +0300

    Update hw04_normal.py

--- lesson04/home_work/hw04_normal.py
+++ lesson04/home_work/hw04_normal.py
@@ -1,6 +1,11 @@
 # Задание-1:
 # Вывести символы в нижнем регистре, которые окружают 1 или более символа в верхнем регистре.
 # Решить задачу двумя способами: с помощью re и без.
+
+import re
+
+"""К сожалению, не догадалась, как не выводить отсутствующие символы"""
+
 line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNOGIPHpEMujalp' \
        'PLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewVzKTUfSYtBydXaVIpxWjNKgXANv' \
        'IoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSAHqnLxooisBDWuxIhyjJaXDYwdoVPnsllMngNlmkp' \
@@ -13,10 +18,32 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNOGIP
        'nZjLeMiFlxnPkqfJFbCfKCuUJmGYJZPpRBFNLkqigxFkrRAppYRXeSCBxbGvqHmlsSZMWSVQyzenWoGxy' \
        'GPvbnhWHuXBqHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQNJFaXi' \
        'UWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
+       
+low_abc = re.findall(r'([a-z]?)[A-Z]+([a-z])', line)
+print('Первый способ:\n', low_abc)
+
+found = []
+k = 0
+
+"""Использовала цикл while, а не for, чтобы вручную менять счетчик"""
+
+while k < len(line):
+       if line[k].isupper():
+              if line[k-1].islower():
+                     found.append(line[k-1])
+              if line[k+1].islower():
+                     found.append(line[k+1])
+                     k += 2
+       k += 1
+
+print('Второй способ:\n', found)
 
 # Задание-2:
 # Вывести символы в верхнем регистре, которые окружают ровно два символа в нижнем регистре слева
 # и два символа в верхнем регистре справа. Решить задачу двумя способами: с помощью re и без.
+
+print('_____________')
+
 line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNOGIPHpEMujalp' \
        'PLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewVzKTUfSYtBydXaVIpxWjNKgXANv' \
        'IoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSAHqnLxooisBDWuxIhyjJaXDYwdoVPnsllMngNlmkp' \
@@ -30,7 +57,35 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNOG
        'GPvbnhWHuXBqHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQNJFaXi' \
        'UWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
 
+big_letter = re.findall(r'[a-z]{2}([A-Z])[A-Z]{2}', line_2)
+print('Первый способ:\n', big_letter)
+
+not_reg = []
+j = 0
+while j < len(line_2)-2:
+       if line_2[j].isupper():
+              if line_2[j-1].islower() and line_2[j-2].islower() and line_2[j+1].isupper() and line_2[j+2].isupper():
+                     not_reg.append(line_2[j])
+                     j += 4
+       j += 1
+
+print('Второй способ:\n', not_reg)
+
 # Задача-3:
 # Напишите скрипт заполняющий указанный файл (самомстоятельно задайте имя файла) произвольными целыми
 # цифрами, в результате в файле должно быть 2500-значное произвольное число
 # Найдите и выведите самую длинную последовательность одинаковых цифр в вышезаполненном файле
+
+print('_____________')
+
+from random import choice
+from string import digits
+
+"""Не смогла дорешать эту задачу. Файл создается, заполняется цифрами. С нахождением повторений у меян в этот раз не получилось."""
+
+with open('more_digits.txt', 'w+') as f:
+       f.write(''.join(choice(digits) for i in range(2500)))
+       result = f.read()
+       print(result)
+       sequence = re.findall(r'', f)
+       print(sequence)
