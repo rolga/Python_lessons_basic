bcb7eefb70e32cbb4b5204af5c00d535ab8a8471
rolga <glyinderwust@gmail.com>
2017-08-11 17:31:20 +0300

    Update and rename hw06_hard.py to hw06_hard(Rashkueva).py

--- lesson06/home_work/hw06_hard(Rashkueva).py
+++ lesson06/home_work/hw06_hard(Rashkueva).py
@@ -8,3 +8,27 @@
 
 # С использованием классов.
 # Рализуйте классы сотрудников так, чтобы на вход функции-конструктора каждый работник получал строку из файла
+
+with open('data/workers.txt', 'w+', encoding='UTF-8') as f:
+    f.write('Иванов 240 168\nПетров 200 168\nСидоров 250 168\nПупкин 220 168\nКазакова 230 168')
+
+with open('data/workers.txt', 'r', encoding='UTF-8') as f:
+    worker_list = f.read()
+    print(type(worker_list))
+
+with open('data/hours_of.txt', 'w+', encoding='utf-8') as h:
+    h.write('Иванов 168\nПетров 140\nСидоров 180\nПупкин 166\nКазакова 170')
+
+with open('data/hours_of.txt', 'r', encoding='utf-8') as h:
+    hours_list = h.read()
+    print(hours_list)
+
+worker_date = worker_list.split('\n')
+print(worker_date)
+
+class Worker:
+
+    def __init__(self, date):
+        self.date = worker_list[i]
+
+
